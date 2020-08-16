1f39145abe756129df086aec1471aa37c4ca68c9
Pratik Gole <pratikgole5682@gmail.com>
2020-08-16 19:41:50 +0530

    change1

---
+++ hib3/src/main/java/kk/fetch.java
@@ -0,0 +1,49 @@
+package kk;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+
+
+
+public class fetch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ServiceRegistry ssr= new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory sf=meta.buildSessionFactory();
+	    Session session=sf.openSession();
+		
+		//TypedQuery<store>tt=session.createQuery("from store");
+	    TypedQuery<store>tq=session.createQuery("from store");
+		List<store> ll=tq.getResultList();
+		
+		   Iterator<store>itr=ll.iterator();
+	//	Iterator<store> itr=ll.iterator();
+		
+		while(itr.hasNext())
+		{ 	
+			 store ss=itr.next();
+			
+			System.out.println(" id="+ss.getId());
+			System.out.println("\n");
+		//	System.out.println(" name="+ss.getName());
+		}
+		
+		System.out.println("came");
+		
+		
+	}
+
+}
---
+++ hib3/src/main/java/kk/main.java
@@ -0,0 +1,51 @@
+package kk;
+
+import javax.transaction.Transaction;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		store str=new store(0, null);
+		
+		str.setId(42);
+		str.setName("sonuuu");
+		
+		ServiceRegistry ssr= new  StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+	Metadata meta= new MetadataSources(ssr).getMetadataBuilder().build();
+		
+		SessionFactory sf =meta.buildSessionFactory();
+		
+		Session ss=sf.openSession();
+		org.hibernate.Transaction  tx= ss.beginTransaction();
+		
+		ss.save(str);
+		
+		tx.commit();
+		ss.close();
+		System.out.println(" data updated ");
+		
+		
+		//Exception in thread "main" org.hibernate.internal.util.config.ConfigurationException: Unable to perform unmarshalling at line number 0 and column 0 
+		//in RESOURCE hibernate.cfg.xml. Message: null
+		
+		// if this error comes go and check the    
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
